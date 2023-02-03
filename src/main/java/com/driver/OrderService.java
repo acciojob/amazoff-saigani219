f44548ce274d393db4dab3714b4a66226ee400da
sai ganesh <saigani1002@gmail.com>
2023-02-03 18:49:02 +0530

    New Delivery Service on the way

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class OrderService {
 
 	@Autowired
-	OrderRepository orderRepository;
+	OrderRepository orderRepository = new OrderRepository();
 
 	public void addOrder(Order order) {
 		orderRepository.addOrder(order);
